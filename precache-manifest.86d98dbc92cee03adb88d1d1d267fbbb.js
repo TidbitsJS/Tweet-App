63fc95afae82b2b050048e5bb60eba8088ca25f4
sujata <gunalesujata@gmail.com>
2020-07-31 21:57:51 +0530

    Updates

---
+++ precache-manifest.86d98dbc92cee03adb88d1d1d267fbbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02179b906b8f97724462dc9bcce1c858",
+    "url": "/Tweet-App/index.html"
+  },
+  {
+    "revision": "cc69a45367f64bcd0592",
+    "url": "/Tweet-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a051372a9a068d5358ab",
+    "url": "/Tweet-App/static/css/main.30ab368b.chunk.css"
+  },
+  {
+    "revision": "cc69a45367f64bcd0592",
+    "url": "/Tweet-App/static/js/2.6bb2392a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Tweet-App/static/js/2.6bb2392a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a051372a9a068d5358ab",
+    "url": "/Tweet-App/static/js/main.b05e316d.chunk.js"
+  },
+  {
+    "revision": "b1d79e5d2eb1a4737ddf",
+    "url": "/Tweet-App/static/js/runtime-main.eaef9002.js"
+  }
+]);
